7d7982ee60163083fa29f0bcf2b10c92a10a062c
sourcejon <jon@thesource.co.uk>
2021-02-23 10:37:01 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+﻿function setupPaymentModal(clientKey, code, dotNetObject) {
+    var foo = Worldpay.useTemplateForm({
+        'clientKey': clientKey,
+        'form': 'paymentForm',
+        'paymentSection': 'paymentSection',
+        'display': 'inline',
+        'code': code,
+        'callback': function (obj) {
+            if (obj && obj.token) {
+                dotNetObject.invokeMethodAsync("SubmitPaymentForm", obj.token);
+            }
+        }
+    });
+}
